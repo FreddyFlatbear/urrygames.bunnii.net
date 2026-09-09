f87a5c6ce980d0e85f276e967888cb248da390c4
Violet <68258126+VioletDotVR@users.noreply.github.com>
2026-09-09 13:52:00 +1200

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -3,7 +3,7 @@ import { initLanyard } from './status.js';
 // API Configuration Map
 const API_CONFIG = {
     nsHost: 'https://ns.bunnii.net',
-    profileId: 'amebunnii',
+    profileId: 'ownerprofile',
     productId: 'product_5cafa9c5-b53b-439a-8639-79ed8766f5d2',
     timeout: 5000,
     retryAttempts: 2
